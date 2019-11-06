bc1e4a330b5ac41a715df20b9c43e8113b54ffc7
akrishna77 <arkr94@gmail.com>
2019-11-06 17:06:32 -0500

    Changing to InceptionResnetV2 (keras updates)

--- dataset/utils/inception_utils.py
+++ dataset/utils/inception_utils.py
@@ -1,13 +1,15 @@
 from keras import Model
 from keras.models import load_model
-from keras.applications.inception_resnet_v2 import preprocess_input
+from keras.applications.inception_resnet_v2 import InceptionResNetV2
+from keras.applications.imagenet_utils import preprocess_input
 import numpy as np
 import keras.backend as K
 from dataset.utils.shared import checkpoint_url
 
 K.clear_session()
-x = load_model(checkpoint_url)
-y = x.get_layer('predictions').output
+x = InceptionResNetV2(include_top=True, weights='imagenet')
+# load_model(checkpoint_url)
+y = x.output
 model = Model(inputs=x.input, outputs=y)
 
 
